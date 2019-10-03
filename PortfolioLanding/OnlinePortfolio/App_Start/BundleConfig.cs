8463f59e0775858e36855c226a37f145f9616207
d3m1rt45 <d3m1rt45@outlook.com>
2019-10-03 21:29:08 +0100

    Sidebar 0.1
    Problems with Sidebar Implementation.
    
    Revise this tomorrow and decide whether it is more appropriate to look for another solution for page's navigation.

--- PortfolioLanding/OnlinePortfolio/App_Start/BundleConfig.cs
+++ PortfolioLanding/OnlinePortfolio/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace OnlinePortfolio
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jQuery/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jQuery/jquery.validate*"));
